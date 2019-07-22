b79e1089ab913f85cd14931316971e5f62f91876
Karlo Krakan <karlo@krakan.ca>
2019-07-22 16:44:49 -0700

    stats page changes

--- app/stats/stats-view-model.js
+++ app/stats/stats-view-model.js
@@ -8,8 +8,11 @@ function StatsViewModel(args) {
 		page: args,
 		stat: "weight",
 		title: "Weight",
+		unit: "lbs",
 		stats: ["weight", "chest", "arms", "waist", "hips", "butt"],
 		graphStats: [],
+		graphMin: 0,
+		graphMax: 0,
 		items: [],
 		switchStat: function (args) {
 			for (x in viewModel.stats) {
@@ -21,21 +24,45 @@ function StatsViewModel(args) {
 			btn.borderWidth = "6px";
 			viewModel.set("stat", stat);
 			viewModel.set("title", this.capitalizeFirst(stat));
+			this.stat == "weight" ? this.unit = "lbs" : this.unit = "cm";
 			this.getGraphStats();
 		},
 		getGraphStats() {
 			statsService.getGraphStats({
 				stat: this.stat
 			}).then(function (data) {
-				properJSON = jsonB(data, {dropQuotesOnNumbers: true, dropQuotesOnKeys: true, minify: true});
+				properJSON = jsonB(data, {
+					dropQuotesOnNumbers: true,
+					dropQuotesOnKeys: true,
+					minify: true
+				});
 				console.log(properJSON);
+				var min = 1000;
+				var max = 0;
+				var keys = Object.keys(data);
+				for (var i = 0; i < keys.length; i++) {
+					var key = keys[i];
+					var inner = Object.keys(data[key]);
+					var innerKey = inner[1];
+					if (data[key][innerKey] < min) {
+						min = data[key][innerKey]
+					}
+					if (data[key][innerKey] > max) {
+						max = data[key][innerKey]
+					}
+				}
+				console.log("min: " + min);
+				console.log("max: " + max);
+				viewModel.set("graphMin", min - 3);
+				viewModel.set("graphMax", max + 3);
 				viewModel.set("graphStats", data);
 			})
 		},
 		capitalizeFirst(str) {
 			return str.charAt(0).toUpperCase() + str.slice(1);
 		}
-    	
+
+
 	})
 
 	// Run on page load:
@@ -43,40 +70,31 @@ function StatsViewModel(args) {
 
 	viewModel.page.getViewById("weight").borderWidth = "6px";
 
-	viewModel.items.push(
-        {
-            itemName: "Arcade Fire",
-            itemDesc: "Funeral"
-        },
-        {
-            itemName: "Bon Iver",
-            itemDesc: "For Emma, Forever Ago"
-        },
-        {
-            itemName: "Daft Punk",
-            itemDesc: "Random Access Memories"
-        },
-        {
-            itemName: "Elbow",
-            itemDesc: "Build a Rocket Boys!"
-		},
-		{
-            itemName: "Arcade Fire",
-            itemDesc: "Funeral"
-        },
-        {
-            itemName: "Bon Iver",
-            itemDesc: "For Emma, Forever Ago"
-        },
-        {
-            itemName: "Daft Punk",
-            itemDesc: "Random Access Memories"
-        },
-        {
-            itemName: "Elbow",
-            itemDesc: "Build a Rocket Boys!"
-        }
-	)
+	viewModel.items.push({
+		itemName: "Arcade Fire",
+		itemDesc: "Funeral"
+	}, {
+		itemName: "Bon Iver",
+		itemDesc: "For Emma, Forever Ago"
+	}, {
+		itemName: "Daft Punk",
+		itemDesc: "Random Access Memories"
+	}, {
+		itemName: "Elbow",
+		itemDesc: "Build a Rocket Boys!"
+	}, {
+		itemName: "Arcade Fire",
+		itemDesc: "Funeral"
+	}, {
+		itemName: "Bon Iver",
+		itemDesc: "For Emma, Forever Ago"
+	}, {
+		itemName: "Daft Punk",
+		itemDesc: "Random Access Memories"
+	}, {
+		itemName: "Elbow",
+		itemDesc: "Build a Rocket Boys!"
+	})
 
 	return viewModel;
 }
